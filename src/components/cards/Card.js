b71f2626d469bfea1c5ba42dd6b4bc600097f425
Ritik <ritikpr307@gmail.com>
2021-05-17 19:35:05 +0530

    Created Homepage

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,14 +1,26 @@
-import { Body, Heading, SubHeading } from "./styles";
+// Stlying
+// Icons
+import { CgProfile as Profile } from "react-icons/cg";
+import { AiOutlineClockCircle as Time } from "react-icons/ai";
+import {
+  Body, Heading, SubHeading, Btn, Description, LeftSection, RightSection,
+} from "./styles";
 
 function Card() {
   return (
     <Body>
-      <Heading>Blog Title</Heading>
+      <Heading>ProjectSakura April 2021 Security update is here.</Heading>
       <hr />
-      <SubHeading>Name</SubHeading>
-      <p>
-        Blog content
-      </p>
+      <SubHeading>
+        <LeftSection><Profile />  LordShenron</LeftSection>
+        <RightSection><Time /> April 12, 2021</RightSection>
+      </SubHeading>
+      <Description>
+        Whats new in the latest release of Project Sakura? Check out..
+      </Description>
+      <Btn inverted color="blue">
+        Read More
+      </Btn>
     </Body>
   );
 }
