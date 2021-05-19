1e90a1ee6eed9e4f475992ed1d33ee7143c5a15f
Ritik <ritikpr307@gmail.com>
2021-05-19 21:17:17 +0530

    added pagination

---
+++ src/components/Pagination/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+// semantic-ui
+import { Container, Button } from "semantic-ui-react";
+
+export const PageBtn = styled(Button)`
+    border: 1px solid white;
+    margin: auto 1vh;
+    padding: 1vh;
+`;
+export const PageGroup = styled(Container)`
+    text-align: center;
+    margin: 2vh auto;
+`;
--- src/components/routes/Home/Home.js
+++ src/components/routes/Home/Home.js
@@ -1,22 +1,38 @@
-import { Container } from "semantic-ui-react";
-import Card from "../../cards/Card";
+import { useState, useEffect } from "react";
+import Pagination from "../../Pagination/Pagination";
+import Posts from "../../Posts/Posts";
+
 //card data
 import cardData from "../../data/cardData";
 // styles
 import { Body } from "./styles";
 
 function Home() {
-  // console.log(cardData);
-  const renderCards = () => {
-    const cards = cardData.map((data) => <Card data={data} key={data.author} />);
-    return cards;
-  };
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(5);
+
+  useEffect(() => {
+    setLoading(true);
+    setPosts(cardData);
+    setLoading(false);
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <Body>
-      <Container>
-        {renderCards()}
-      </Container>
+      <Posts cardData={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </Body>
   );
 }
