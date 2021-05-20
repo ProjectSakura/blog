8cb1bbb752c52e401a69218d0dc52c596ea62d0c
Ritik <ritikpr307@gmail.com>
2021-05-20 21:55:10 +0530

    added responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,36 @@
 // styles
-import {
-  Body, Image, Menu, MenuItem,
-} from "./styles";
+import { NavMenu,Body,NavMenuItem, Image,DropDown,DropDownItem,DropDownMenu } from "./styles";
 // Logo
 import Logo from "../../images/logo.png";
+import {  Menu } from "semantic-ui-react";
+import { useState } from "react";
 
 function Navbar() {
-  return (
-    <Body>
-      <Image src={Logo} />
-      <Menu>
-        <MenuItem>Home</MenuItem>
-        <MenuItem>Blog</MenuItem>
-        <MenuItem>Stats</MenuItem>
-        <MenuItem>Twitter</MenuItem>
-        <MenuItem>Telegram</MenuItem>
-      </Menu>
-    </Body>
-  );
+    const [activeItem, setActive] = useState("home");
+    const home="https://projectsakura.github.io/";
+    const stats="https://projectsakura.github.io/stats.html";
+    const twitter="https://twitter.com/ProjectSakura_";
+    const telegram="https://t.me/ProjectSakura";
+    return (
+        <NavMenu secondary >
+            <Menu.Menu>
+                <Image src={Logo}></Image>
+            </Menu.Menu>
+            <Menu.Menu position="right">
+                <NavMenuItem href={home} name="Home"/>
+                <NavMenuItem href={stats} name="Stats"/>
+                <NavMenuItem href={twitter} name="Twitter"/>
+                <NavMenuItem href={telegram} name="Telegram"/>
+                <DropDown item icon="bars" simple>
+                    <DropDownMenu>
+                        <DropDownItem  href={home}>Home</DropDownItem>
+                        <DropDownItem  href={stats}>Stats</DropDownItem>
+                        <DropDownItem  href={twitter}>Twitter</DropDownItem>
+                        <DropDownItem  href={telegram}>Telegram</DropDownItem>
+                    </DropDownMenu>
+                </DropDown>
+            </Menu.Menu>
+        </NavMenu>
+    );
 }
 export default Navbar;
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,20 +1,109 @@
+import { Dropdown, Menu } from "semantic-ui-react";
 import styled from "styled-components";
 
-export const Body = styled.div`
-    padding: 1.5vh;
-    background-color: #1C1C24;
-`;
 export const Image = styled.img`
     border-radius: 2em;
-    width:6vh;
-    height:6vh;
+    width: 6vh;
+    height: 6vh;
 `;
-export const Menu = styled.div`
-    margin-top: 2vh;
-    float:right;
+export const NavMenu = styled(Menu)`
+    background-color: #1c1c24 !important;
+    padding: 1vh 2vh !important;
 `;
-export const MenuItem = styled.span`
+export const NavMenuItem = styled(Menu.Item)`
+    @media (max-width: 768px) {
+        display: none !important;
+    }
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
     font-size: 1.2em;
-    margin: auto 2vh;
-    
+    color: white !important;
+    &:hover {
+        color: #53c497 !important;
+        cursor: pointer;
+    }
 `;
+export const DropDown = styled(Dropdown)`
+    @media (max-width: 768px) {
+        display: block !important;
+    }
+    display: none !important;
+    color: white !important;
+    font-size: 1.2em !important;
+`;
+export const DropDownMenu = styled(Dropdown.Menu)`
+    /* background-color: gray !important; */
+`;
+export const DropDownItem = styled(Dropdown.Item)`
+    /* background-color: gray !important;    */
+`;
+
+// export const MenuItem = styled.span`
+//     @media (max-width: 768px) {
+//         display: none;
+//     }
+//     font-size: 1.2em;
+//     margin: auto 2vh;
+// `;
+
+// export const DropDown = styled(Dropdown)`
+//     @media (max-width: 768px) {
+//         display: flex !important;
+//         flex-direction: column;
+//     }
+//     font-size: 1.5em !important;
+//     display: none !important;
+
+// `;
+// export const DropDownMenu = styled(Dropdown.Menu)`
+
+// `;
+// export const DropDown = styled.ul`
+//     float: right;
+//     background-color: #1C1C24;
+//     list-style: none;
+// 	margin: 0;
+// 	padding-left: 0;
+// `
+
+// export const DropDownItem = styled.li`
+//     color: #fff;
+//     background: #1C1C24;
+// 	display: block;
+// 	float: left;
+// 	padding: 1rem;
+// 	position: relative;
+// 	text-decoration: none;
+//     transition-duration: 0.5s;
+//     &:hover,:focus-within {
+//         background: red;
+//         cursor: pointer;
+//     }
+// `;
+
+// export const DropDownGroup = styled.ul`
+//     background: orange;
+// 	visibility: hidden;
+//     opacity: 0;
+//     min-width: 5rem;
+// 	position: absolute;
+//     transition: all 0.5s ease;
+//     margin-top: 1rem;
+// 	left: 0;
+//     display: none;
+//     &:hover,:focus{
+//         visibility: visible;
+//         opacity: 1;
+//         display: block;
+//     }
+// `;
+// export const DropDownHeading = styled.li`
+
+// `;
+// export const DropDownMenu = styled.ul`
+//     &:hover ${DropDownGroup}{
+//         visibility: visible;
+//         opacity: 1;
+//         display: block;
+//     }
+// `;
