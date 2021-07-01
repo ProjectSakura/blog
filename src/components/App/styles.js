05a1da5feaff0de77b8cd55f2f1c58635f7be818
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-07-01 10:51:03 +0530

    Fixed image issue and third-party overhead

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -2,11 +2,18 @@ import styled from "styled-components";
 import { Loader } from "semantic-ui-react";
 
 export const Body = styled.div`
+    @font-face {
+        font-family: 'Pacifico';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Pacifico Regular'), local('Pacifico-Regular'), url(https://fonts.gstatic.com/s/pacifico/v12/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2) format('woff2');
+        font-display: swap;
+    }
     height:100% ;
-    font-display: swap;
-    /* background-color:#121217; */
+    background-color:#121217;
     color:white;
 `;
 export const LoaderDiv = styled(Loader)`
+    background-color: white !important;
     margin-top: 30vh !important;
 `;
--- src/components/routes/Home/Style.js
+++ src/components/routes/Home/Style.js
@@ -3,6 +3,13 @@ import { Loader } from "semantic-ui-react";
 import BgImg from "../../../images/blogs/recent.webp";
 
 export const Container = styled.div`
+    @font-face {
+        font-family: 'Pacifico';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Pacifico Regular'), local('Pacifico-Regular'), url(https://fonts.gstatic.com/s/pacifico/v12/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2) format('woff2');
+        font-display: swap;
+    }
     @media (min-width: 900px) {
         /* display: flex; */
         height: 100%;
